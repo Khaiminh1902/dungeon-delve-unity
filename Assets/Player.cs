8a6dfd15a1efd2ba921e87e098270e7d106c1f12
Khaiminh1902 <khaiminhnguyen1902@gmail.com>
2025-08-15 11:28:04 +0700

    Completed movements (both WASD nad arrow keys)

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,5 +2,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   
+    private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed = 3.5f;
+    private float xInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
 }
