d05aa0b6c1ef3576fb4054cc252a2302847f4237
Khaiminh1902 <khaiminhnguyen1902@gmail.com>
2025-08-16 19:02:14 +0700

    Ughhh

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,10 +4,18 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
+    [Header("Movements details")]
     [SerializeField] private float moveSpeed = 7f;
-    [SerializeField] private float jumpForce = 8;
+    [SerializeField] private float jumpForce = 15;
     private float xInput;
-    [SerializeField] private bool facingRight = true;
+    private bool facingRight = true;
+    [Header("Collision details")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+    
+
+
 
     private void Awake()
     {
@@ -17,6 +25,7 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        HandleCollision();
         xInput = Input.GetAxisRaw("Horizontal");
 
         HandleMovement();
@@ -34,6 +43,7 @@ public class Player : MonoBehaviour
             Jump();
         }
         HandleAnimations();
+        HandleFlip();
     }
 
     private void HandleAnimations()
@@ -50,10 +60,27 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        if(isGrounded)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+    private void HandleFlip()
+    {
+        if (rb.linearVelocity.x > 0 && facingRight == false)
+            Flip();
+        else if (rb.linearVelocity.x < 0 && facingRight == true)
+            Flip();
     }
     private void Flip()
     {
         transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, -groundCheckDistance));
     }
 }
