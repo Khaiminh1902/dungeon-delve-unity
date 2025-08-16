7a9b3d53cf085b2f0891d88ffb4254d4c69172f5
Khaiminh1902 <khaiminhnguyen1902@gmail.com>
2025-08-16 19:01:54 +0700

    Completed run animations and flip. Fixed infinite jump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
-    [SerializeField] private float moveSpeed = 3.5f;
+    private Animator anim;
+    [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 8;
-
     private float xInput;
+    [SerializeField] private bool facingRight = true;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -31,6 +33,14 @@ public class Player : MonoBehaviour
         {
             Jump();
         }
+        HandleAnimations();
+    }
+
+    private void HandleAnimations()
+    {
+        bool isMoving = rb.linearVelocity.x != 0;
+
+        anim.SetBool("isMoving", isMoving);
     }
 
     private void HandleMovement()
@@ -41,5 +51,9 @@ public class Player : MonoBehaviour
     private void Jump()
     {
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+    private void Flip()
+    {
+        transform.Rotate(0, 180, 0);
     }
 }
